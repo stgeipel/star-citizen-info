adb6b6e7a350fc1507ea3bb1dabbdd7c7fb53448
Steffen Geipel <geipel.steffen@gmail.com>
2020-06-24 22:37:29 +0200

    adding swr

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,19 @@
 import Head from 'next/head'
 import { useState } from 'react'
+import useSWR from 'swr'
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-}
+const fetcher = (url) => fetch(url).then((r) => r.json())
 
 const Home = () => {
+  const { data, error } = useSWR('/api/places', fetcher)
+
   return (
     <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      {data && data.places.map((d) => <span>{d.name}</span>)}
       <main></main>
     </div>
   )
